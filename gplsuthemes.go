e6a3f64487ed96ab01ee6c670eb186db43036cc9
Scott Detweiler <scott.detwiler@gmail.com>
2015-09-10 16:25:50 -0500

    working images

--- gplsuthemes.go
+++ gplsuthemes.go
@@ -11,21 +11,21 @@ import (
 
 type ThemeList struct {
 	XMLName xml.Name `xml:"Themes"`
-	Themes []Theme `xml:"Theme"`
+	Themes  []Theme `xml:"Theme"`
 }
 
 type Theme struct {
 	XMLName xml.Name `xml:"Theme"`
-	Name string `xml:"Name,attr"`
-	Page string `xml:"Page,attr"`
-	Tag string `xml:"Tag,attr"`
-	Day string `xml:"Day,attr"`
+	Name    string `xml:"Name,attr"`
+	Page    string `xml:"Page,attr"`
+	Tag     string `xml:"Tag,attr"`
+	Day     string `xml:"Day,attr"`
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    template,_ := template.ParseFiles("templates/listing.html")
-    themedata := openXML("db/themedata.xml")
-    template.Execute(w, themedata)
+	template, _ := template.ParseFiles("templates/listing.html")
+	themedata := openXML("db/themedata.xml")
+	template.Execute(w, &themedata)
 }
 
 // Fetch the current XML document and return the Theme[]
@@ -33,23 +33,30 @@ func openXML(filename string) ThemeList {
 
 	xmlFile, err := os.Open(filename)
 	if err != nil {
-	    fmt.Println("Error opening file:", err)
+		fmt.Println("Error opening file:", err)
 	}
 	defer xmlFile.Close()
-
 	XMLdata, _ := ioutil.ReadAll(xmlFile)
-	
-	var t ThemeList
 
+	var t ThemeList
 	xml.Unmarshal(XMLdata, &t)
 
 	return t
 }
 
+func serveSingle(pattern string, filename string) {
+	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+			http.ServeFile(w, r, filename)
+		})
+}
 
 func main() {
 
 	http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+
+	serveSingle("/style.css", "./templates/style.css")
+	serveSingle("/images/url.png", "./templates/images/url.png")
+
+	http.ListenAndServe(":8080", nil)
 
 }
